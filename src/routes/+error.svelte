1659a61e979977c8cf9971813be4e79947a2b8c0
matchascript <38482672+MatchaScript@users.noreply.github.com>
2024-03-16 21:23:26 +0900

    Add error page fallback

---
+++ src/routes/+error.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<main>
+  <h1>{$page.status} {$page.error.message}</h1>
+  <a href="/">Back to home</a>
+  <div class="blank" />
+</main>
+
+<style lang="postcss">
+  h2,
+  h1 {
+    margin-top: calc(var(--gap) * 2);
+    text-align: center;
+  }
+  a {
+    text-align: center;
+    margin-top: var(--gap);
+    color: var(--accent);
+  }
+  .blank {
+    height: 25vh;
+  }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -19,7 +19,16 @@ const config = {
   ],
 
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      fallback: '404.html',
+    }),
+    prerender: {
+      entries: [
+        '*',
+        '/archive/page/*',
+        '/tags/*/*',
+      ]
+    }
   }
 };
 
